d3272f0e0bd76ca849cc949923b8a5c219a15484
agrube@gmail.com <agrube@gmail.com@02c0af4c-466e-5418-7ffd-d4ece9d06713>
2011-01-30 01:22:02 +0000

    Obstacle/Door combined to make some more sense. Backgrounds, animations etc added

--- trunk/Platformer/Platformer/Platformer/Door.cs
+++ trunk/Platformer/Platformer/Platformer/Door.cs
@@ -9,33 +9,90 @@ namespace Platformer
     /// <summary>
     /// This is a game component that implements IUpdateable.
     /// </summary>
-    class Door : Obstacle
+    class Door
     {
-        public Door(Level level, Vector2 position)
-            : base(level, position)
+        private Level level;
+        private Vector2 origin;
+        private Vector2 basePosition;
+        private Texture2D texture;
+        private bool passable;
+        //private Tile obTile;
+
+        public bool Passable
         {
-            Passable = false;
+            get { return passable; }
+            set { passable = value; }
         }
 
-        public override void Update(Player player, GameTime gameTime)
+        //public Tile ObTile
+        //{
+        //    get { return obTile; }
+        //    set { obTile = value; }
+        //}
+
+        public Level Level
+        {
+            get { return level; }
+        }
+
+        public Vector2 Origin
+        {
+            get { return origin; }
+            set { origin = value; }
+        }
+
+        public Vector2 Position
+        {
+            get
+            {
+                return basePosition;
+            }
+        }
+
+        public Texture2D Texture
+        {
+            get { return texture; }
+            set { texture = value; }
+        }
+
+        public Circle BoundingCircle
+        {
+            get
+            {
+                return new Circle(Position, Tile.Width / 3.0f);
+            }
+        }
+
+        public Door(Level level, Vector2 position)
+        {
+            this.level = level;
+            this.basePosition = position;
+
+            LoadContent();
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, Position, null, Color.White, 0.0f, origin, 1.0f, SpriteEffects.None, 0.0f);
+        }
+
+        public void Update(Player player, GameTime gameTime)
         {
             if (player.ageState >= 1)
             {
                 Passable = true;
-                //Tile.Collision = TileCollision.Passable;
             }
             else
             {
                 Passable = false;
-                //Tile.Collision = TileCollision.Impassable;
             }
         }
 
-        public override void LoadContent()
+        public void LoadContent()
         {
+            Passable = false;
             Texture = Level.Content.Load<Texture2D>("Tiles/Tile_Door");
             Origin = new Vector2(Texture.Width / 2.0f, Texture.Height / 2.0f);
         }
-
     }
 }
